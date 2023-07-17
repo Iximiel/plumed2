a28f24db51576b2b9a45cabd07d78c2a5fcd2c4e
Daniele Rapetti <daniele.rapetti@sissa.it>
2023-07-17 11:19:10 +0200

    moved the templateness of the Loop Unroller to the single functions, added inline to the functions

--- src/tools/LoopUnroller.h
+++ src/tools/LoopUnroller.h
@@ -47,111 +47,117 @@ Implementation is made using template metaprogramming, that is:
 Here xxx is any of the methods of the class.
 
 */
-template<unsigned n>
-class LoopUnroller {
-public:
+
+namespace LoopUnroller {
 /// Set to zero.
 /// Same as `for(unsigned i=0;i<n;i++) d[i]=0.0;`
-  static void _zero(double*d);
+template<unsigned n>  static void _zero(double*d);
 /// Add v to d.
 /// Same as `for(unsigned i=0;i<n;i++) d[i]+=v[i];`
-  static void _add(double*d,const double*v);
+template<unsigned n> static void _add(double*d,const double*v);
 /// Subtract v from d.
 /// Same as `for(unsigned i=0;i<n;i++) d[i]-=v[i];`
-  static void _sub(double*d,const double*v);
+template<unsigned n>  static void _sub(double*d,const double*v);
 /// Multiply d by s.
 /// Same as `for(unsigned i=0;i<n;i++) d[i]*=s;`
-  static void _mul(double*d,const double s);
+template<unsigned n>  static void _mul(double*d,const double s);
 /// Set d to -v.
 /// Same as `for(unsigned i=0;i<n;i++) d[i]=-v[i];`
-  static void _neg(double*d,const double*v);
+template<unsigned n>  static void _neg(double*d,const double*v);
 /// Squared modulo of d;
 /// Same as `r=0.0; for(unsigned i=0;i<n;i++) r+=d[i]*d[i]; return r;`
-  static double _sum2(const double*d);
+template<unsigned n>  static double _sum2(const double*d);
 /// Dot product of d and v
 /// Same as `r=0.0; for(unsigned i=0;i<n;i++) r+=d[i]*v[i]; return r;`
-  static double _dot(const double*d,const double*v);
-};
+template<unsigned n>  static double _dot(const double*d,const double*v);
+}
 
 template<unsigned n>
-void LoopUnroller<n>::_zero(double*d) {
-  LoopUnroller<n-1>::_zero(d);
+inline
+void LoopUnroller::_zero(double*d) {
+  LoopUnroller::_zero<n-1>(d);
   d[n-1]=0.0;
 }
 
 template<>
 inline
-void LoopUnroller<1>::_zero(double*d) {
+void LoopUnroller::_zero<1>(double*d) {
   d[0]=0.0;
 }
 
 template<unsigned n>
-void LoopUnroller<n>::_add(double*d,const double*a) {
-  LoopUnroller<n-1>::_add(d,a);
+inline
+void LoopUnroller::_add(double*d,const double*a) {
+  LoopUnroller::_add<n-1>(d,a);
   d[n-1]+=a[n-1];
 }
 
 template<>
 inline
-void LoopUnroller<1>::_add(double*d,const double*a) {
+void LoopUnroller::_add<1>(double*d,const double*a) {
   d[0]+=a[0];
 }
 
 template<unsigned n>
-void LoopUnroller<n>::_sub(double*d,const double*a) {
-  LoopUnroller<n-1>::_sub(d,a);
+inline
+void LoopUnroller::_sub(double*d,const double*a) {
+  LoopUnroller::_sub<n-1>(d,a);
   d[n-1]-=a[n-1];
 }
 
 template<>
 inline
-void LoopUnroller<1>::_sub(double*d,const double*a) {
+void LoopUnroller::_sub<1>(double*d,const double*a) {
   d[0]-=a[0];
 }
 
 template<unsigned n>
-void LoopUnroller<n>::_mul(double*d,const double s) {
-  LoopUnroller<n-1>::_mul(d,s);
+inline
+void LoopUnroller::_mul(double*d,const double s) {
+  LoopUnroller::_mul<n-1>(d,s);
   d[n-1]*=s;
 }
 
 template<>
 inline
-void LoopUnroller<1>::_mul(double*d,const double s) {
+void LoopUnroller::_mul<1>(double*d,const double s) {
   d[0]*=s;
 }
 
 template<unsigned n>
-void LoopUnroller<n>::_neg(double*d,const double*a ) {
-  LoopUnroller<n-1>::_neg(d,a);
+inline
+void LoopUnroller::_neg(double*d,const double*a ) {
+  LoopUnroller::_neg<n-1>(d,a);
   d[n-1]=-a[n-1];
 }
 
 template<>
 inline
-void LoopUnroller<1>::_neg(double*d,const double*a) {
+void LoopUnroller::_neg<1>(double*d,const double*a) {
   d[0]=-a[0];
 }
 
 template<unsigned n>
-double LoopUnroller<n>::_sum2(const double*d) {
-  return LoopUnroller<n-1>::_sum2(d)+d[n-1]*d[n-1];
+inline
+double LoopUnroller::_sum2(const double*d) {
+  return LoopUnroller::_sum2<n-1>(d)+d[n-1]*d[n-1];
 }
 
 template<>
 inline
-double LoopUnroller<1>::_sum2(const double*d) {
+double LoopUnroller::_sum2<1>(const double*d) {
   return d[0]*d[0];
 }
 
 template<unsigned n>
-double LoopUnroller<n>::_dot(const double*d,const double*v) {
-  return LoopUnroller<n-1>::_dot(d,v)+d[n-1]*v[n-1];
+inline
+double LoopUnroller::_dot(const double*d,const double*v) {
+  return LoopUnroller::_dot<n-1>(d,v)+d[n-1]*v[n-1];
 }
 
 template<>
 inline
-double LoopUnroller<1>::_dot(const double*d,const double*v) {
+double LoopUnroller::_dot<1>(const double*d,const double*v) {
   return d[0]*v[0];
 }
 
