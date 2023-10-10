6c5034127ba8498156760c841bb375628e038019
Daniele Rapetti <daniele.rapetti@sissa.it>
2023-10-10 09:41:14 +0200

    test now check that the results for initialization are the same with the "simpler" algortithm

--- regtest/basic/rt-NeigbourlistInitialization/main.cpp
+++ regtest/basic/rt-NeigbourlistInitialization/main.cpp
@@ -12,6 +12,8 @@ using PLMD::Pbc;
 
 // Testing that the Neigbour list will be intialized with the desired number of
 // couples
+// We are initializing with distance and stride not set to check the default
+// parameters
 
 #define check(arg) (((arg)) ? "pass\n" : "not pass\n")
 
@@ -22,7 +24,7 @@ int main(int, char **) {
   Communicator cm{};
   bool serial = true;
   bool do_pbc = false;
-  for (size_t nat0 : {100, 500, 1000, 10000}) {
+  for (const size_t nat0 : {100, 500, 1000, 10000}) {
     std::vector<AtomNumber> list0(nat0);
     size_t i = 0;
     for (auto &an : list0) {
@@ -31,27 +33,33 @@ int main(int, char **) {
     }
     {
       report << "Single list:\n";
+      std::string prepend="["+std::to_string(nat0)+"]";
       size_t expected = ((nat0 - 1) * nat0) / 2;
       auto nl = NeighborList(list0, serial, do_pbc, pbc, cm);
             
       bool expectedcouples = true;
-      for (size_t cID = 0; cID < expected && expectedcouples; ++cID) {
-        unsigned ii = expected - 1 - cID;
-        unsigned K =
-            unsigned(std::floor((std::sqrt(double(8 * ii + 1)) + 1) / 2));
-        unsigned jj = ii - K * (K - 1) / 2;
-        auto couple = nl.getClosePair(cID);
-        expectedcouples &= couple.first == nat0 - 1 - K;
-        expectedcouples &= couple.second == nat0 - 1 - jj;
-      }report << "[" << nat0<< "]Initial number:      "
+      {
+        size_t cID = 0;
+        for (size_t i0 = 0; i0 < nat0 && expectedcouples; ++i0) {
+          for (size_t i1 = i0+1; i1 < nat0 && expectedcouples; ++i1) {  
+            auto couple = nl.getClosePair(cID);
+            expectedcouples &= couple.first == i0;
+            expectedcouples &= couple.second == i1;
+            ++cID;
+          }
+        }
+      }
+      report << prepend << "Initial number:      "
         << check(nl.size() == expected);
-      report << "[" << nat0 << "]Initialized couples: "
+      report << prepend << "getIndexPair():      "
         << check(expectedcouples);
-      report << "[" << nat0 << "]Lastupdate is 0:     "
+      report << prepend << "Lastupdate is 0:     "
         << check(nl.getLastUpdate() == 0);
+      report << prepend << "Default stride is 0: "
+          << check(nl.getStride() == 0);
       report << "\n";
     }
-    for (size_t nat1 : {100, 500, 1000, 10000}) {
+    for (const size_t nat1 : {100, 500, 1000, 10000}) {
 
       std::vector<AtomNumber> list1(nat1);
 
@@ -63,43 +71,60 @@ int main(int, char **) {
 
       {
         report << "Double list, no pairs:\n";
+        std::string prepend="["+std::to_string(nat0)
+            + ", " + std::to_string(nat1)  +"]";
         bool do_pair = false;
         size_t expected = nat1 * nat0;
         auto nl = NeighborList(list0, list1, serial, do_pair, do_pbc, pbc, cm);
                 
         bool expectedcouples = true;
-        for (size_t cID = 0; cID < expected && expectedcouples; ++cID) {
-          auto couple = nl.getClosePair(cID);
-          expectedcouples &= couple.first == (cID / nat1);
-          expectedcouples &= couple.second == (cID % nat1 + nat0);
+        {
+          size_t cID = 0;
+          for (size_t i0 = 0; i0 < nat0 && expectedcouples; ++i0) {
+            for (size_t i1 = 0; i1 < nat1 && expectedcouples; ++i1) {
+              auto couple = nl.getClosePair(cID);
+              //The getIndexPair for non couple input must return this be this
+              //(cID / nat1);
+              expectedcouples &= couple.first == i0;
+              //(cID % nat1 + nat0);
+              expectedcouples &= couple.second == nat0+i1;
+              ++cID;
+            }
+          }
         }
-        report << "[" << nat0 << ", " << nat1 << "]Initial number:      "
+        report << prepend << "Initial number:      "
           << check(nl.size() == expected);
-        report << "[" << nat0 << ", " << nat1 << "]Initialized couples: "
+        report << prepend << "getIndexPair():      "
           << check(expectedcouples);
-        report << "[" << nat0 << ", " << nat1 << "]Lastupdate is 0:     "
+        report << prepend << "Lastupdate is 0:     "
           << check(nl.getLastUpdate() == 0);
+        report << prepend << "Default stride is 0: "
+          << check(nl.getStride() == 0);
         report << "\n";
       }
 
       if (nat1 == nat0) {
         report << "Double list, with pairs:\n";
+        std::string prepend="["+std::to_string(nat0)
+            + ", " + std::to_string(nat1) +"]";
         bool do_pair = true;
-        size_t expected = nat1;
+        size_t expected = nat0;
         auto nl = NeighborList(list0, list1, serial, do_pair, do_pbc, pbc, cm);
         
         bool expectedcouples = true;
-        for (size_t cID = 0; cID < expected && expectedcouples; ++cID) {
+        for (size_t cID = 0; cID < nat0 && expectedcouples; ++cID) {
           auto couple = nl.getClosePair(cID);
           expectedcouples &= couple.first == cID;
           expectedcouples &= couple.second == cID + nat0;
         }
-        report << "[" << nat0 << ", " << nat1 << "]Initial number:      " 
+        report << prepend << "Initial number:      " 
           << check(nl.size() == expected);
-        report << "[" << nat0 << ", " << nat1 << "]Initialized couples: " 
+        report << prepend << "getIndexPair():      "
           << check(expectedcouples);
-        report << "[" << nat0 << ", " << nat1 << "]Lastupdate is 0:     " 
+        report << prepend <<  "Lastupdate is 0:     " 
           << check(nl.getLastUpdate() == 0);
+        report << prepend << "Default stride is 0: "
+          << check(nl.getStride() == 0);
         report << "\n";
       }
     }
