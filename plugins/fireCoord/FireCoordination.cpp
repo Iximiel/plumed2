86bb7844d0be0bd1787d6c5bb4936f9c29ad95b0
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2024-02-27 10:40:47 +0100

    setting up fireCoord
    setting up tests
    
    compiles!
    
    need to set up derivatives for pair coordination
    
    derivatives sorta works, need to set up the stretch
    
    Added the virial
    
    better tests, now I do no divide by 1
    
    corrected stretch, memory management, virial

---
+++ plugins/fireCoord/FireCoordination.cpp
@@ -0,0 +1,469 @@
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   Copyright (c) 2024 The plumed team
+   (see the PEOPLE file at the root of the distribution for a list of names)
+
+   See http://www.plumed.org for more information.
+
+   This file is part of plumed, version 2.
+
+   plumed is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   plumed is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with plumed.  If not, see <http://www.gnu.org/licenses/>.
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+//not using #ifdef __PLUMED_HAS_ARRAYFIRE, we assume that af is presetn
+
+#include <arrayfire.h>
+#ifdef __PLUMED_HAS_ARRAYFIRE_CUDA
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+#include <af/cuda.h>
+namespace afdevice=afcu;
+#elif __PLUMED_HAS_ARRAYFIRE_OCL
+#include <af/opencl.h>
+namespace afdevice=afocl;
+#else
+//this is a small workaround to use less #ifdef ;)
+namespace afdevice {
+int getNativeId(int const i) { return i;}
+}
+#endif
+
+
+#include "core/ActionRegister.h"
+#include "tools/Communicator.h"
+#include "core/PlumedMain.h"
+#include "colvar/Colvar.h"
+
+#include <limits>
+#include <iostream>
+
+#define vdbg(...) std::cerr << __LINE__ << ":" << #__VA_ARGS__ << " " << (__VA_ARGS__) << '\n'
+// #define vdbg(...)
+namespace PLMD {
+namespace colvar {
+
+//TODO: this is identical to the plain cuda implementation
+template <typename calculateFloat> struct rationalSwitchParameters {
+  calculateFloat dmaxSQ = std::numeric_limits<calculateFloat>::max();
+  calculateFloat invr0_2 = 1.0; // r0=1
+  calculateFloat stretch = 1.0;
+  calculateFloat shift = 0.0;
+  int nn = 6;
+  int mm = 12;
+};
+
+template <typename calculateFloat>
+std::pair<af::array,af::array> fastRationalN2M(const af::array& distSquared,
+    const rationalSwitchParameters<calculateFloat> switchingParameters) {
+  auto NN = switchingParameters.nn / 2;
+  auto rdistSQ = distSquared * switchingParameters.invr0_2;
+  auto rNdist = af::pow(rdistSQ, NN - 1);
+  auto res =  calculateFloat(1.0) / (calculateFloat(1.0) + rdistSQ * rNdist);
+  //need to use select
+  auto dfunc = rNdist * res * res *
+               (-NN * 2.0
+                * switchingParameters.invr0_2 * switchingParameters.stretch);
+
+  return std::make_pair(
+           res * switchingParameters.stretch + switchingParameters.shift,
+           dfunc);
+}
+
+//stolen from my CUDA implementation:
+struct DataInterface {
+  // NOT owning pointer
+  double *ptr = nullptr;
+  size_t size = 0;
+  DataInterface() = delete;
+
+  // VectorGeneric is a "memory map" on an n linear array
+  // &vg[0] gets the pointer to the first double in memory
+  // C++ vectors align memory so we can safely use the vector variant of
+  // DataInterface
+  template <unsigned n>
+  explicit DataInterface (PLMD::VectorGeneric<n> &vg)
+    : ptr (&vg[0]), size (n) {}
+  // TensorGeneric is a "memory map" on an n*m linear array
+  // &tg[0][0] gets  the pointer to the first double in memory
+  // C++ vectors align memory so we can safely use the vector variant of
+  // DataInterface
+  template <unsigned n, unsigned m>
+  explicit DataInterface (PLMD::TensorGeneric<n, m> &tg)
+    : ptr (&tg[0][0]), size (n * m) {}
+  template <typename T>
+  explicit DataInterface (std::vector<T> &vt) : DataInterface (vt[0]) {
+    size *= vt.size();
+  }
+};
+
+template <typename calculateFloat> class FireCoordination : public Colvar {
+
+  unsigned atomsInA = 0;
+  unsigned atomsInB = 0;
+  int  deviceid=-1;
+
+  PLMD::colvar::rationalSwitchParameters<calculateFloat> switchingParameters;
+  bool pbc{true};
+  // void setUpPermanentGPUMemory();
+
+  enum class calculationMode { self, dual, pair, none };
+  calculationMode mode = calculationMode::none;
+  // size_t doSelf();
+  // size_t doDual();
+  // size_t doPair();
+
+public:
+  explicit FireCoordination (const ActionOptions &);
+  virtual ~FireCoordination()=default;
+  // active methods:
+  static void registerKeywords (Keywords &keys);
+  void calculate() override;
+};
+using FireCoordination_d = FireCoordination<double>;
+using FireCoordination_f = FireCoordination<float>;
+PLUMED_REGISTER_ACTION (FireCoordination_d, "FIRECOORDINATION")
+PLUMED_REGISTER_ACTION (FireCoordination_f, "FIRECOORDINATIONFLOAT")
+
+// template <typename calculateFloat>
+// void FireCoordination<calculateFloat>::setUpPermanentGPUMemory() {
+//   auto nat = getPositions().size();
+//   cudaPositions.resize (3 * nat);
+//   cudaDerivatives.resize (3 * nat);
+//   cudaTrueIndexes.resize (nat);
+//   std::vector<unsigned> trueIndexes (nat);
+//   for (size_t i = 0; i < nat; ++i) {
+//     trueIndexes[i] = getAbsoluteIndex (i).index();
+//   }
+//   cudaTrueIndexes = trueIndexes;
+// }
+
+template <typename calculateFloat>
+void FireCoordination<calculateFloat>::registerKeywords (Keywords &keys) {
+  Colvar::registerKeywords (keys);
+
+  // keys.add ("optional", "THREADS", "The upper limit of the number of threads");
+  keys.add ("atoms", "GROUPA", "First list of atoms");
+  keys.add ("atoms", "GROUPB", "Second list of atoms, optional");
+  keys.addFlag ("PAIR",
+                false,
+                "Pair only 1st element of the 1st group with 1st element in "
+                "the second, etc");
+
+  keys.add (
+    "compulsory", "NN", "6", "The n parameter of the switching function ");
+  keys.add ("compulsory",
+            "MM",
+            "0",
+            "The m parameter of the switching function; 0 implies 2*NN");
+  keys.add ("compulsory", "R_0", "The r_0 parameter of the switching function");
+  keys.add (
+    "compulsory", "D_MAX", "0.0", "The cut off of the switching function");
+  keys.add("compulsory","DEVICEID","-1","Identifier of the GPU to be used");
+
+}
+template <typename calculateFloat>
+FireCoordination<calculateFloat>::FireCoordination (const ActionOptions &ao)
+  : PLUMED_COLVAR_INIT (ao) {
+  std::vector<AtomNumber> GroupA;
+  parseAtomList ("GROUPA", GroupA);
+  std::vector<AtomNumber> GroupB;
+  parseAtomList ("GROUPB", GroupB);
+
+  if (GroupB.size() == 0) {
+    mode = calculationMode::self;
+    atomsInA = GroupA.size();
+  } else {
+    mode = calculationMode::dual;
+    atomsInA = GroupA.size();
+    atomsInB = GroupB.size();
+    bool dopair = false;
+    parseFlag ("PAIR", dopair);
+    if (dopair) {
+      if (GroupB.size() == GroupA.size()) {
+        mode = calculationMode::pair;
+      } else {
+        error ("GROUPA and GROUPB must have the same number of atoms if the "
+               "PAIR keyword is present");
+      }
+    }
+    GroupA.insert (GroupA.end(), GroupB.begin(), GroupB.end());
+  }
+  if (mode == calculationMode::none) {
+    error (
+      R"(implementation error in constructor: calculation mode cannot be "none")");
+  }
+  bool nopbc = !pbc;
+  parseFlag ("NOPBC", nopbc);
+  pbc = !nopbc;
+
+  // parse ("THREADS", maxNumThreads);
+  // if (maxNumThreads <= 0) {
+  //   error ("THREADS should be positive");
+  // }
+  addValueWithDerivatives();
+  setNotPeriodic();
+  requestAtoms (GroupA);
+
+  log.printf ("  \n");
+  if (pbc) {
+    log.printf ("  using periodic boundary conditions\n");
+  } else {
+    log.printf ("  without periodic boundary conditions\n");
+  }
+  if (mode == calculationMode::pair) {
+    log.printf ("  with PAIR option\n");
+  }
+  std::string sw, errors;
+
+  { // loading data to the GPU
+    int nn_ = 6;
+    int mm_ = 0;
+
+    calculateFloat r0_ = 0.0;
+    parse ("R_0", r0_);
+    if (r0_ <= 0.0) {
+      error ("R_0 should be explicitly specified and positive");
+    }
+
+    parse ("NN", nn_);
+    parse ("MM", mm_);
+    if (mm_ == 0) {
+      mm_ = 2 * nn_;
+    }
+    if (mm_ % 2 != 0 || mm_ % 2 != 0)
+      error (" this implementation only works with both MM and NN even");
+    constexpr auto d0_ = 0.0;
+
+
+    switchingParameters.nn = nn_;
+    switchingParameters.mm = mm_;
+    switchingParameters.stretch = 1.0;
+    switchingParameters.shift = 0.0;
+
+    calculateFloat dmax = 0.0;
+    parse ("D_MAX", dmax);
+    if (dmax == 0.0) { // TODO:check for a "non present flag"
+      // set dmax to where the switch is ~0.00001
+      dmax = r0_ * std::pow (0.00001, 1.0 / (nn_ - mm_));
+      // ^This line is equivalent to:
+      // SwitchingFunction tsw;
+      // tsw.set(nn_,mm_,r0_,0.0);
+      // dmax=tsw.get_dmax();
+      // in plain plumed
+    }
+
+    switchingParameters.dmaxSQ = dmax * dmax;
+    calculateFloat invr0 = 1.0 / r0_;
+    switchingParameters.invr0_2 = invr0 * invr0;
+    constexpr bool dostretch = true;
+    if (dostretch) {
+      std::array<calculateFloat,2> inputs = {0.0, dmax};
+      //fastRationalN2M gets the square as input
+      inputs[1] *= inputs[1];
+      af::array AFinputs(2,1,inputs.data());
+      auto [res, dfunc] = fastRationalN2M(AFinputs,switchingParameters);
+      std::array<calculateFloat,2> resZeroMax;
+      res.host(resZeroMax.data());
+
+      switchingParameters.stretch = 1.0 / (resZeroMax[0] - resZeroMax[1]);
+      switchingParameters.shift = -resZeroMax[1] * switchingParameters.stretch;
+    }
+  }
+
+  checkRead();
+
+  // setUpPermanentGPUMemory();
+
+  // maxReductionNumThreads = min (1024, maxNumThreads);
+
+  // cudaFuncAttributes attr;
+  // the kernels are heavy on registers, this adjust the maximum number of
+  // threads accordingly
+  // switch (mode) {
+  // case calculationMode::self:
+  //   if (pbc) {
+  //     cudaFuncGetAttributes (&attr, &getSelfCoord<true, calculateFloat>);
+  //   } else {
+  //     cudaFuncGetAttributes (&attr, &getSelfCoord<false, calculateFloat>);
+  //   }
+  //   break;
+  // case calculationMode::dual:
+  //   if (pbc) {
+  //     cudaFuncGetAttributes (&attr, &getDerivDual<true, calculateFloat>);
+  //     maxNumThreads = min (attr.maxThreadsPerBlock, maxNumThreads);
+  //     cudaFuncGetAttributes (&attr, &getCoordDual<true, calculateFloat>);
+  //   } else {
+  //     cudaFuncGetAttributes (&attr, &getDerivDual<false, calculateFloat>);
+  //     maxNumThreads = min (attr.maxThreadsPerBlock, maxNumThreads);
+  //     cudaFuncGetAttributes (&attr, &getCoordDual<false, calculateFloat>);
+  //   }
+  //   break;
+  // case calculationMode::pair:
+  //   if (pbc) {
+  //     cudaFuncGetAttributes (&attr, &getCoordPair<true, calculateFloat>);
+  //   } else {
+  //     cudaFuncGetAttributes (&attr, &getCoordPair<false, calculateFloat>);
+  //   }
+  //   break;
+  // case calculationMode::none:
+  //   // throw"this should not have been happened"
+  //   break;
+  // }
+  // maxNumThreads = min (attr.maxThreadsPerBlock, maxNumThreads);
+
+  log << "  contacts are counted with cutoff (dmax)="
+      << sqrt (switchingParameters.dmaxSQ)
+      << ",\nwith a rational switch with parameters: d0=0.0, r0="
+      << 1.0 / sqrt (switchingParameters.invr0_2)
+      << ", N=" << switchingParameters.nn << ", M=" << switchingParameters.mm
+      << ".\n";
+
+  parse("DEVICEID",deviceid);
+  if(comm.Get_rank()==0) {
+    // if not set try to check the one set by the API
+    if(deviceid==-1) deviceid=plumed.getGpuDeviceId();
+    // if still not set use 0
+    if(deviceid==-1) deviceid=0;
+
+    af::setDevice(afdevice::getNativeId(deviceid));
+    af::info();
+//   log << "GPU info:\n"
+//       << "\t max threads per coordination" << maxNumThreads << "\n"
+//       << "\t max threads per reduction" << maxReductionNumThreads << "\n";
+  }
+}
+
+template <typename calculateFloat>
+constexpr af_dtype getType() {
+  return  af_dtype::f32;
+}
+
+template <>
+constexpr af_dtype getType<double>() {
+  return  af_dtype::f64;
+}
+
+template <typename calculateFloat>
+inline af::array setPositions(const double* const data, const size_t size) {
+  std::vector<float> posi(3*size);
+  for (unsigned i=0; i<size; ++i) {
+    posi[3*i]   = static_cast<float>(data[3*i] );
+    posi[3*i+1] = static_cast<float>(data[3*i+1]);
+    posi[3*i+2] = static_cast<float>(data[3*i+2]);
+  }
+  return af::array(3, size, &posi.front());
+}
+
+template<>
+inline af::array setPositions<double>(const double* const data, const size_t size) {
+  return af::array(3, size, data);
+}
+
+
+template <typename calculateFloat>
+inline void getToHost(const af::array& input,
+                      DataInterface destination) {
+  auto const size = input.elements();
+  if(size >(destination.size) ) {
+    plumed_merror("PLMD->AF::getToHost(): destination cannot contain all the data ("+
+                  std::to_string(size) + " > " + std::to_string(destination.size) +")");
+  }
+  std::vector<float> buffer(size);
+  input.host(buffer.data());
+  for (unsigned i=0; i<size; ++i) {
+    destination.ptr[i] =  static_cast<double>(buffer[i]);
+  }
+}
+
+template <>
+inline void getToHost<double>(const af::array& input,
+                              DataInterface destination) {
+  auto const size = input.elements();
+  if(size >(destination.size) ) {
+    plumed_merror("PLMD->AF::getToHost(): destination cannot contain all the data ("+
+                  std::to_string(size) + " > " + std::to_string(destination.size) +")");
+  }
+  input.host(destination.ptr);
+}
+
+template <typename calculateFloat>
+void FireCoordination<calculateFloat>::calculate () {
+  // auto positions = getPositions();
+
+  double coordination;
+  auto derivativeA = std::vector<Vector> (atomsInA);
+  auto derivativeB = std::vector<Vector> (atomsInB);
+  PLMD::Tensor virial;
+  { //PAIR
+    auto posA = setPositions<calculateFloat>(&getPositions()[0][0], atomsInA);
+    auto posB = setPositions<calculateFloat>(&getPositions()[atomsInA][0], atomsInB);
+    auto diff = posB - posA;
+
+    auto ddistSQ = af::sum(diff * diff);
+    //now we discard the distances that are greater than the limit
+    auto keys = ddistSQ < switchingParameters.dmaxSQ;
+    auto [res, dfunc] = fastRationalN2M(ddistSQ, switchingParameters);
+    auto AFderiv = af::select(keys, dfunc * diff, 0.0);
+
+    auto AFvirial=af::array(9,atomsInA,getType<calculateFloat>());
+    AFvirial.row(0) = AFderiv.row(0) * diff.row(0);
+    AFvirial.row(1) = AFderiv.row(0) * diff.row(1);
+    AFvirial.row(2) = AFderiv.row(0) * diff.row(2);
+    AFvirial.row(3) = AFderiv.row(1) * diff.row(0);
+    AFvirial.row(4) = AFderiv.row(1) * diff.row(1);
+    AFvirial.row(5) = AFderiv.row(1) * diff.row(2);
+    AFvirial.row(6) = AFderiv.row(2) * diff.row(0);
+    AFvirial.row(7) = AFderiv.row(2) * diff.row(1);
+    AFvirial.row(8) = AFderiv.row(2) * diff.row(2);
+    //we constructed an
+    //dx*dfunc dx*dfunc dx*dfunc dz*dfunc dz*dfunc dz*dfunc dy*dfunc dy*dfunc dy*dfunc
+    //tensor, and we multiply it by the tensor:
+    //dx dy dz dx dy dz dx dy dz
+    //"external product"
+
+    //no need to lookup/select, already done in  the deriv
+    AFvirial = -af::sum(AFvirial, 1);
+    // AFvirial = -af::sum(af::lookup(AFvirial,af::where(keys),1),1);
+    getToHost<calculateFloat>(AFvirial, DataInterface(virial));
+
+    calculateFloat t;
+    //need to measure which one is faster:
+    af::sum(af::lookup(res, af::where(keys))).host(&t);
+    // af::sum(af::select(keys,res,0.0)).host(&t);
+    coordination = t;
+
+    getToHost<calculateFloat>(-AFderiv, DataInterface(derivativeA));
+    getToHost<calculateFloat>( AFderiv, DataInterface(derivativeB));
+
+    // auto tmp =af::where(!keys);
+    // vdbg(tmp.dims());
+    // std::vector<char> tt(keys.dims()[1]);
+    // keys.host(tt.data());
+    // for(auto i=0;i<tt.size();++i){
+    //   std::cerr << i << " " << derivativeA[i] << "\n";
+    // }
+    // vdbg(tmp.type());
+  }
+
+  for(unsigned i=0u; i < atomsInA ; ++i) {
+    setAtomsDerivatives (i, derivativeA[i]);
+  }
+  for(unsigned i=0u; i < atomsInB; ++i) {
+    setAtomsDerivatives (atomsInA+i, derivativeB[i]);
+  }
+  setValue (coordination);
+  setBoxDerivatives (virial);
+}
+} // namespace colvar
+} // namespace PLMD
+
