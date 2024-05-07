a75f16a7e3e64f39e782553a461cc209d1574973
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2024-05-07 14:42:20 +0200

    works, sorta

--- plugins/openACCCoord/fastCoord.cxx
+++ plugins/openACCCoord/fastCoord.cxx
@@ -77,67 +77,103 @@ float calculateSwitch(unsigned const natA,
   float const stretch=1.0/(s0-sd);
   float const shift=-sd*stretch;
   const unsigned nat = natA+natB;
-  vdbg(nat);
   float ncoord=0.0f;
+    
 #pragma acc data copy(ncoord) \
-        copyin(positions[:3*nat]) \
-        copyout(derivatives[:3*nat])
+        copyin(positions[0:3*nat]) \
+        copyout(derivatives[0:3*nat],virial[0:9])
   {
-    if (natB==0) {
-#pragma acc parallel loop gang reduction(+:ncoord)
+#pragma acc parallel
+{
+     virial[0] = 0.0f;//0.0f;
+     virial[1] = 0.0f;//1.0f;
+     virial[2] = 0.0f;//2.0f;
+    virial[3] = 0.0f;//3.0f;
+     virial[4] = 0.0f;//4.0f;
+     virial[5] = 0.0f;//5.0f;
+     virial[6] = 0.0f;//6.0f;
+     virial[7] = 0.0f;//7.0f;
+     virial[8] = 0.0f;//8.0f;
+}   
+    
+    if (natB==0) {//self
+#pragma acc parallel loop gang reduction(+:ncoord,virial[0:9])
       for (size_t i = 0; i < natA; i++) {
-        const unsigned atomA=i;
         float myNcoord=0.0f;
 
         float mydevX=0.0f;
         float mydevY=0.0f;
         float mydevZ=0.0f;
 
-// #pragma acc loop seq
-#pragma acc loop vector reduction(+:myNcoord,mydevX,mydevY,mydevZ)
-        for (size_t j = 0; j < natA; j++) {
-          const unsigned atomB=j;
+        float myVirial_0=0.0f;
+        float myVirial_1=0.0f;
+        float myVirial_2=0.0f;
+        float myVirial_3=0.0f;
+        float myVirial_4=0.0f;
+        float myVirial_5=0.0f;
+        float myVirial_6=0.0f;
+        float myVirial_7=0.0f;
+        float myVirial_8=0.0f;
 
-          const float d0=positions[3*atomB  ]-positions[3*atomA  ];
-          const float d1=positions[3*atomB+1]-positions[3*atomA+1];
-          const float d2=positions[3*atomB+2]-positions[3*atomA+2];
+//this needs some more work to functionc correctly
+// #pragma acc loop worker reduction(+:myNcoord,mydevX,mydevY,mydevZ, \
+//         myVirial_0,myVirial_1,myVirial_2, \
+//         myVirial_3,myVirial_4,myVirial_5, \
+//         myVirial_6,myVirial_7,myVirial_8)
+#pragma acc loop seq
+        for (size_t j = 0; j < natA; j++) {
+
+          const float d0=positions[3*j  ]-positions[3*i  ];
+          const float d1=positions[3*j+1]-positions[3*i+1];
+          const float d2=positions[3*j+2]-positions[3*i+2];
 
           float dsq=d0*d0+d1*d1+d2*d2;
           //todo this:
           //if(getAbsoluteIndex(i0)==getAbsoluteIndex(i1)) continue;
 
-
-          //add will need to be the check on the same id
+          //add will need to be the check on the same "real" id
           bool add=i!=j;
           auto [t,dfunc ]=calculateSqr<6>(dsq,invr0_2,dmaxsq, stretch,shift);
           myNcoord +=add*t;
 
           // dfunc*=add;
 
-          float t_0 = -dfunc * d0;
-          float t_1 = -dfunc * d1;
-          float t_2 = -dfunc * d2;
+          const float t_0 = -dfunc * d0;
+          const float t_1 = -dfunc * d1;
+          const float t_2 = -dfunc * d2;
           mydevX += t_0;
           mydevY += t_1;
           mydevZ += t_2;
           add&=i>j;
-          // myVirial_0 += t_0 * d0*add;
-          // myVirial_1 += t_0 * d1*add;
-          // myVirial_2 += t_0 * d2*add;
-          // myVirial_3 += t_1 * d0*add;
-          // myVirial_4 += t_1 * d1*add;
-          // myVirial_5 += t_1 * d2*add;
-          // myVirial_6 += t_2 * d0*add;
-          // myVirial_7 += t_2 * d1*add;
-          // myVirial_8 += t_2 * d2*add;
+          myVirial_0 += t_0 * d0 * add;
+          myVirial_1 += t_0 * d1 * add;
+          myVirial_2 += t_0 * d2 * add;
+          myVirial_3 += t_1 * d0 * add;
+          myVirial_4 += t_1 * d1 * add;
+          myVirial_5 += t_1 * d2 * add;
+          myVirial_6 += t_2 * d0 * add;
+          myVirial_7 += t_2 * d1 * add;
+          myVirial_8 += t_2 * d2 * add;
         }
         ncoord += 0.5 * myNcoord;
-        derivatives[3*atomA+0] = mydevX;
-        derivatives[3*atomA+1] = mydevY;
-        derivatives[3*atomA+2] = mydevZ;
+        virial[0] += myVirial_0;
+        virial[1] += myVirial_1;
+        virial[2] += myVirial_2;
+        virial[3] += myVirial_3;
+        virial[4] += myVirial_4;
+        virial[5] += myVirial_5;
+        virial[6] += myVirial_6;
+        virial[7] += myVirial_7;
+        virial[8] += myVirial_8;
+        
+        derivatives[3*i+0] = mydevX;
+        derivatives[3*i+1] = mydevY;
+        derivatives[3*i+2] = mydevZ;
       }
-    }
-  }
+    }//self
+        
+  } //data clause
+
   return ncoord;
 }
 
